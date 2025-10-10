3c887fd96bed898a1cff3b133c9686dee9fdb4e2
Tomek4861 <Tomek4861g@gmail.com>
2025-10-10 11:24:45 +0200

    Update URL patterns & add api to navbar

--- sampleapp/urls.py
+++ sampleapp/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeAPIView.as_view(), name="home"),
-    path('user', views.user_delete, name='user_delete'),
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('api/', views.HomeAPIView.as_view(), name='api_root'),
+    path('api/user', views.user_delete, name='user_delete'),
     path('contact', views.contact_page, name='contact'),
     path('portfolio/', views.portfolio_page, name='portfolio')
     # path('portfolio/photo/<int:photo_id>/', views.portfolio_page, name='portfolio')
